e0074298cecf7c3e31024044758b241d74b8d67e
ThureinS <thureinjarvis@gmail.com>
2025-01-09 13:21:30 +0630

    Limit job listing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
-import Navbar from '@/components/Navbar.vue';
-import Hero from '@/components/Hero.vue';
-import HomeCards from '@/components/HomeCards.vue';
-import JobListings from './components/JobListings.vue';
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "./components/JobListings.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero />
   <HomeCards />
-  <JobListings />
+  <JobListings limit="3" />
 </template>
 
 <style></style>
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,20 +1,27 @@
 <script setup>
-    import jobData from "@/jobs.json";
-    import {ref} from 'vue';
-    import JobListing from "@/components/JobListing.vue";
+import jobData from "@/jobs.json";
+import { ref } from "vue";
+import JobListing from "@/components/JobListing.vue";
 
-    const jobs = ref(jobData);
+const jobs = ref(jobData);
+defineProps({
+  limit: Number,
+});
 </script>
 
 <template>
-    <section class="bg-blue-50 px-4 py-10">
-        <div class="container-xl lg:container m-auto">
-            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
-                Browse Jobs
-            </h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs" :key="job.id" :job="job"/>
-            </div>
-        </div>
-    </section>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing
+          v-for="job in jobs.slice(0, limit)"
+          :key="job.id"
+          :job="job"
+        />
+      </div>
+    </div>
+  </section>
 </template>
